5135913c3c104164992465f83f9e460b36859f62
AmmadJavaid006 <ammad.javaid420@gmail.com>
2024-07-12 23:33:01 +0500

    Added New File Py_dict_file.py

---
+++ Py_dict_file.py
@@ -0,0 +1,21 @@
+fname = input("Enter File Name: ")
+fhandle = open(fname)
+lst = list()
+count = dict()
+big_val = 0
+
+for addr in fhandle:
+    if addr.startswith("From") and not addr.startswith("From:"):
+        addr = addr.split()
+        lst.append(addr[1])
+    else:
+        continue
+for mail in lst:
+        count[mail] = count.get(mail, 0) + 1
+    
+big_val = max(count.values())
+
+key = {i for i in count if count[i] == big_val}
+
+for finalmail in key:
+    print(finalmail, big_val)
