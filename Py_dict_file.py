eb26f52eedfd70cca76cc73477c5dd9207476110
AmmadJavaid006 <ammad.javaid420@gmail.com>
2024-07-13 20:13:40 +0500

    Added File Py_time_count.py

--- Py_dict_file.py
+++ Py_dict_file.py
@@ -3,6 +3,7 @@ fhandle = open(fname)
 lst = list()
 count = dict()
 big_val = 0
+small_val = 0
 
 for addr in fhandle:
     if addr.startswith("From") and not addr.startswith("From:"):
@@ -14,11 +15,19 @@ for mail in lst:
         count[mail] = count.get(mail, 0) + 1
     
 big_val = max(count.values())
+small_val = min(count.values())
 
 #key = {i for i in count if count[i] == big_val}
 
 for i in count:
-     if count[i] == big_val:
-          key = i
+    if count[i] == big_val:
+        big_key = i
+    if count[i] == small_val:
+         small_key = i
+
+""" for z in count:
+    if count[z] == small_val:
+        small_key = z  """
           
-print(key, big_val)
+print(big_key, big_val)
+print(small_key, small_val)
---
+++ Py_time_count.py
@@ -0,0 +1,20 @@
+fname = input("Enter File Name: ")
+fhandle = open(fname)
+lst = list()
+count = dict()
+finallist = list()
+for line in fhandle:
+    if line.startswith("From") and not line.startswith("From:"):
+        line = line.split()
+        lst.append(line[5])
+
+for hour in lst:
+    hour = hour.split(":")
+    finallist.append(hour[0])
+
+for ftime in finallist:
+    count[ftime] = count.get(ftime, 0) + 1
+
+
+for x, y in sorted(count.items()):
+    print(x, y)
